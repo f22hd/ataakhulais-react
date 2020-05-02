595f8fee1b467464b3092aaad1c3d576f6644ad2
fahd_allebdi <fallebdi@channels.com.sa>
2020-05-02 04:51:38 +0300

    hiding sensitive data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import LandingPage from "./screens/landing-page";
 import Header from "./components/layout/header";
--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,9 +1,8 @@
-// const baseUrl = "http://127.0.0.1:8000/api";
-const baseUrl = "http://ataakhulais-api.allebdi.me/api";
+const baseUrl = process.env.REACT_APP_ENDPOINT_URL;
 
 const constants = {
   Headers: {
-    API_KEY: "NvNhzRLGdgl0PV862L121ifAFOFWCtonv6VA8yCujrZLDidULh",
+    API_KEY: process.env.REACT_APP_KEY,
     API_VERSION: "1.0",
     Api_Lang: "ar",
     CONTENT_TYPE: "application/x-www-form-urlencoded",
